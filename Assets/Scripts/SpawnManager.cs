b09bc382b369447f9004cbf6e9b88884c33853db
Todd Albert <toddalbert@gmail.com>
2025-03-06 09:09:30 -0500

    spawn powerups; assets cleanup

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,34 +1,45 @@
 using UnityEngine;
+using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _enemyPrefabs;
     [SerializeField] private GameObject _enemyContainer;
+    [SerializeField] private GameObject _powerUpPrefab;
 
     private float _spawnRangeX = 10;
     private float _spawnPosY = 7.8f;
-
     private bool _stopSpawning = false;
-
-    private float _startDelay = 2;
     private float _spawnInterval = 2.5f;
 
     void Start()
     {
-        InvokeRepeating("SpawnRandomEnemy", _startDelay, _spawnInterval); // coroutine would allow us to stop proccess
+        // InvokeRepeating("SpawnRandomEnemy", _startDelay, _spawnInterval); // coroutine would allow us to stop proccess
+        StartCoroutine(SpawnRandomEnemy());
+        StartCoroutine(SpawnPowerUp());
     }
 
-    void SpawnRandomEnemy()
+    IEnumerator SpawnRandomEnemy()
     {
-        if (_stopSpawning)
-        {
-            return;
+        while (!_stopSpawning)
+        {       
+            int index = Random.Range(0,_enemyPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), _spawnPosY, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefabs[index], spawnPos, _enemyPrefabs[index].transform.rotation);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(_spawnInterval);
         }
-        int index = Random.Range(0,_enemyPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), _spawnPosY, 0);
+    }
 
-        GameObject newEnemy = Instantiate(_enemyPrefabs[index], spawnPos, _enemyPrefabs[index].transform.rotation);
-        newEnemy.transform.parent = _enemyContainer.transform;
+    IEnumerator SpawnPowerUp()
+    {
+        while (!_stopSpawning)
+        {
+            // every 3-7 seconds spawn in powerup
+            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), _spawnPosY, 0);
+            Instantiate(_powerUpPrefab, spawnPos, _powerUpPrefab.transform.rotation);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
     }
 
     public void OnPlayerDeath()
