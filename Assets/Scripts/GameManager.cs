e9764168e7caf158cdc27fe073cf21b517a64527
Todd Albert <toddalbert@gmail.com>
2025-03-07 09:50:16 -0500

    added main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
         {
-            SceneManager.LoadScene(0); // Current Game Scene
+            SceneManager.LoadScene(1); // Current Game Scene
         }
     }
 
